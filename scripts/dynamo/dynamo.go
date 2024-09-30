89ba7f402ab870bdeeef4bc5cd2f6c7ce9dd95f1
CatBloom <shomail.cat@gmail.com>
2024-10-01 00:53:07 +0900

    AWSdynamoに接続処理

--- scripts/dynamo/dynamo.go
+++ scripts/dynamo/dynamo.go
@@ -3,6 +3,7 @@ package dynamo
 import (
 	"context"
 	"log"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -27,27 +28,31 @@ func (d *dynamoDB) GetClient() *dynamodb.Client {
 }
 
 func connectDynamoDB() *dynamodb.Client {
+	// ローカル環境は、dynamo-localに接続する
+	if os.Getenv("ENV") == "local" {
+		cfg, err := config.LoadDefaultConfig(
+			context.TODO(),
+			config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider("dummy", "dummy", "dummy")),
+			config.WithRegion("ap-northeast-1"),
+		)
+		if err != nil {
+			log.Fatalf("unable to load SDK config, %v", err)
+		}
 
-	// cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"))
-	// if err != nil {
-	// 	log.Fatalf("unable to load SDK config, %v", err)
-	// }
+		svc := dynamodb.NewFromConfig(cfg, func(o *dynamodb.Options) {
+			o.BaseEndpoint = aws.String("http://dynamodb-local:8000")
+		})
+		return svc
+	} else {
+		cfg, err := config.LoadDefaultConfig(
+			context.TODO(),
+			config.WithRegion("ap-northeast-1"),
+		)
+		if err != nil {
+			log.Fatalf("unable to load SDK config, %v", err)
+		}
 
-	// Dynamo localのエンドポイント
-	endpoint := "http://dynamodb-local:8000"
-
-	cfg, err := config.LoadDefaultConfig(
-		context.TODO(),
-		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider("dummy", "dummy", "dummy")),
-		config.WithRegion("ap-northeast-1"),
-	)
-
-	if err != nil {
-		log.Fatalf("unable to load SDK config, %v", err)
+		svc := dynamodb.NewFromConfig(cfg)
+		return svc
 	}
-
-	svc := dynamodb.NewFromConfig(cfg, func(o *dynamodb.Options) {
-		o.BaseEndpoint = aws.String(endpoint)
-	})
-	return svc
 }
