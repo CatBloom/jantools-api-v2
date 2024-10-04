d95fa27e39343135490e3178a262b74f76efe191
CatBloom <shomail.cat@gmail.com>
2024-10-05 06:40:46 +0900

    便利関数をまとめる

--- scripts/utils/common.go
+++ scripts/utils/common.go
@@ -2,14 +2,22 @@ package utils
 
 import (
 	"strings"
+	"time"
 
 	"github.com/google/uuid"
 )
 
+// uuidを作成し、ハイフンを取り除く関数
 func GenerateUUIDWithoutHyphens() (string, error) {
 	uuid, err := uuid.NewRandom()
 	if err != nil {
 		return "", err
 	}
 	return strings.ReplaceAll(uuid.String(), "-", ""), nil
+}
+
+// 現在時間をjstで取得する関数
+func NowJST() time.Time {
+	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
+	return time.Now().In(jst)
 }
--- scripts/utils/date.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-import "time"
-
-func NowJST() time.Time {
-	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
-	return time.Now().In(jst)
-}
